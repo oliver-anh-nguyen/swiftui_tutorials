d35513b6633c8249235fa53d66d58527e9eefaed
tuan.anh.nguyen <oliver.anh.nguyen@gmail.com>
2023-01-23 16:36:57 -0600

    Working with UI Controls

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by AnhNguyen on 23/01/2023.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+    static let `default` = Profile(username: "oliver")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { rawValue }
+    }
+}
